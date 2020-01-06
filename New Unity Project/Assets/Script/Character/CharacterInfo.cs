b7d5e2387619a0e96a55f8e6f2210101bd02a96c
gost996 <gost996@naver.com>
2020-01-06 18:31:37 +0900

    Start new Project

---
+++ New Unity Project/Assets/Script/Character/CharacterInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    public int hp;
+    public int damage;
+    public float walkSpeed;
+    public float attackSpeed;
+    
+    protected virtual void Attack(GameObject _target)
+    {
+        
+    }
+
+    protected virtual void Run()
+    {
+
+    }
+}
---
+++ New Unity Project/Assets/Script/Character/PlayerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : CharacterInfo
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
